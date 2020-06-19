52ed69b7777bd3ffe2d90b5791ae6046e7a3911c
Quoteception <57107547+Quoteception@users.noreply.github.com>
2020-06-19 19:26:55 +0100

    Add files via upload
    Added Confirm if want to do another calc

--- Calc_C.c
+++ Calc_C.c
@@ -31,48 +31,58 @@ double Divi(double x, double y) {
 
 int main() {
 
-	int Op_num;
+	int Op_num, j, cont;
 	double num1, num2, Addi1, Subt1, Mult1, Divi1;
 
+	j = 1;
+
+	while (j < 2) {
 
 
+		printf("Welcome to my super calc MD-300!\n\n");
 
-	printf("Welcome to my super calc MD-300!\n\n");
+		printf("What is the first number?\n");
+		scanf("%lf", &num1);
 
-	printf("What is the first number?\n");
-	scanf("%lf", &num1);
+		printf("What operation would you like to do?\nOption 1: Addition\nOption 2: Subtraction\nOption 3: Multiplacation\nOption 4: Division\n");
+		scanf("%d", &Op_num);
 
-	printf("What operation would you like to do?\nOption 1: Addition\nOption 2: Subtraction\nOption 3: Multiplacation\nOption 4: Division\n");
-	scanf("%d", &Op_num);
-	
-	if (Op_num == 1) {
-		printf("What is the second number?\n");
-		scanf("%lf", &num2);
-		Addi1 = Addi(num1, num2);
-		printf("The Answer is: %f\n", Addi1);
+		if (Op_num == 1) {
+			printf("What is the second number?\n");
+			scanf("%lf", &num2);
+			Addi1 = Addi(num1, num2);
+			printf("The Answer is: %f\n", Addi1);
+		}
+		else if (Op_num == 2) {
+			printf("What is the second number?\n");
+			scanf("%lf", &num2);
+			Subt1 = Subt(num1, num2);
+			printf("The Answer is: %f\n", Subt1);
+		}
+		else if (Op_num == 3) {
+			printf("What is the second number?\n");
+			scanf("%lf", &num2);
+			Mult1 = Mult(num1, num2);
+			printf("The Answer is: %f\n", Mult1);
+		}
+		else if (Op_num == 4) {
+			printf("What is the second number?\n");
+			scanf("%lf", &num2);
+			Divi1 = Divi(num1, num2);
+			printf("The Answer is: %f\n", Divi1);
+		}
+		else {
+			printf("Ivalid Operation!\nPlease enter a number from 1 - 4\n");
+		}
+
+		printf("Would you like to perform another calculation? (Y/N)\n");
+		scanf("%d", &cont);
+		if (cont == 1) {
+			j = 1;
+		} else {
+			j = j++;
+		}
+		
 	}
-	else if (Op_num == 2) {
-		printf("What is the second number?\n");
-		scanf("%lf", &num2);
-		Subt1 = Subt(num1, num2);
-		printf("The Answer is: %f\n", Subt1);
-	}
-	else if (Op_num == 3) {
-		printf("What is the second number?\n");
-		scanf("%lf", &num2);
-		Mult1 = Mult(num1, num2);
-		printf("The Answer is: %f\n", Mult1);
-	}
-	else if (Op_num == 4) {
-		printf("What is the second number?\n");
-		scanf("%lf", &num2);
-		Divi1 = Divi(num1, num2);
-		printf("The Answer is: %f\n", Divi1);
-	}
-	else {
-		printf("Ivalid Operation!\nPlease enter a number from 1 - 4\n");
-	}
-
-
 	return 0;
 }
