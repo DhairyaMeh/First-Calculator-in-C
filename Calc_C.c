d682d03ea06cb67f1b0a5e5d97049d35b109daa7
Dhairya Mehta <72181239+DhairyaMeh@users.noreply.github.com>
2021-10-02 16:32:57 +0530

    Updated Calc_C.c
    It is a better yeat more convenient code use it. And if liked it u can merge it.
    Thank you

--- Calc_C.c
+++ Calc_C.c
@@ -1,101 +1,37 @@
 #include <stdio.h>
 #include <stdlib.h>
-#pragma warning(disable : 4996)
+#include <string.h>
+
+int main(int argc, char *argv[])
+{
+    // You have to create a command line utility to add/subtract/divide/multiply two numbers
+    // First command line argument of your c program must be the operation.
+    // The next arguments being the two numbers. For example:
+    // >>Command.c add 45 4
+    // >>49
+    char * operation;
+    int num1, num2;
+    operation = argv[1];
+    num1 = atoi(argv[2]);
+    num2 = atoi(argv[3]);
+
+    // printf("Operation is %s\n", operation);
+    // printf("Num1 is %d\n", num1);
+    // printf("Num2 is %d\n", num2);
+
+    if(strcmp(operation, "add")==0){
+        printf("%d\n", num1 + num2);
+    }
+    else if(strcmp(operation, "subtract")==0){
+        printf("%d\n", num1 - num2);
+    }
+    else if(strcmp(operation, "multiply")==0){
+        printf("%d\n", num1 * num2);
+    }
+    else if(strcmp(operation, "divide")==0){
+        printf("%d\n", num1 / num2);
+    }
 
 
-
-double Addi(double x, double y) {
-	double z;
-	z = x + y;
-	return (z);
-}
-
-double Subt(double x, double y) {
-	double z;
-	z = x - y;
-	return (z);
-}
-
-double Mult(double x, double y) {
-	double z;
-	z = x * y;
-	return (z);
-}
-
-double Divi(double x, double y) {
-	double z;
-	z = x / y;
-	return(z); 
-}
-
-
-int main() {
-
-	char cont[3];
-	int Op_num, j, k;
-	double num1, num2, Addi1, Subt1, Mult1, Divi1;
-
-	j = 1;
-	k = 1;
-
-	while (j < 2) {
-
-		j = 1;
-		k = 1;
-
-		printf("\nWelcome to my super calc MD-300!\n\n");
-
-		printf("What is the first number?\n");
-		scanf("%lf", &num1);
-
-		printf("What operation would you like to do?\nOption 1: Addition\nOption 2: Subtraction\nOption 3: Multiplacation\nOption 4: Division\n");
-		scanf("%d", &Op_num);
-
-		if (Op_num == 1) {
-			printf("What is the second number?\n");
-			scanf("%lf", &num2);
-			Addi1 = Addi(num1, num2);
-			printf("The Answer is: %f\n", Addi1);
-		}
-		else if (Op_num == 2) {
-			printf("What is the second number?\n");
-			scanf("%lf", &num2);
-			Subt1 = Subt(num1, num2);
-			printf("The Answer is: %f\n", Subt1);
-		}
-		else if (Op_num == 3) {
-			printf("What is the second number?\n");
-			scanf("%lf", &num2);
-			Mult1 = Mult(num1, num2);
-			printf("The Answer is: %f\n", Mult1);
-		}
-		else if (Op_num == 4) {
-			printf("What is the second number?\n");
-			scanf("%lf", &num2);
-			Divi1 = Divi(num1, num2);
-			printf("The Answer is: %f\n", Divi1);
-		}
-		else {
-			printf("Ivalid Operation!\nPlease enter a number from 1 - 4\n");
-		}
-
-		while (k < 2) {
-			printf("Would you like to perform another calculation? (Y/N)\n");
-			scanf("%s", cont);
-			if ( (!strcmp(cont, "y")) ||  (!strcmp(cont, "Y"))  || (!strcmp(cont, "yes")) || (!strcmp(cont, "Yes")) || (!strcmp(cont, "yEs")) || (!strcmp(cont, "YEs")) || (!strcmp(cont, "yeS")) || (!strcmp(cont, "YeS")) || (!strcmp(cont, "yES")) || (!strcmp(cont, "YES")) ) {
-				j = 1;
-				k = k++;
-			}
-			else if ( (!strcmp(cont, "n")) || (!strcmp(cont, "N")) || (!strcmp(cont, "no")) || (!strcmp(cont, "No")) || (!strcmp(cont, "nO")) || (!strcmp(cont, "NO")) ) {
-				j = j++;
-				k = k++;
-			}
-			else {
-				k = 1;
-				printf("Invalid Answer!\nPlease Try Again.\n");
-			}
-		}
-		
-	}
-	return 0;
+    return 0;
 }
